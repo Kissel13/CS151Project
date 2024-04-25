67fea286bd0c231f1f5eab13991017b5e58ca832
Chance <ctkiscool13@gmail.com>
2024-04-25 12:55:36 -0700

    changes to main and accountManager

--- Project/AccountManager.java
+++ Project/AccountManager.java
@@ -9,7 +9,18 @@ public class AccountManager {
         accountManager = new ArrayList<Account>();
     }
 
+    public Boolean accountVerify(Account user) {
+        // check if account exists in account file
+        return false;
+    }
+
+    public Boolean passwordVerify(Account user) {
+        // check if password matches account in file
+        return false;
+    }
+
     public void addAccount(Account user) {
+        // write new account to file holding accounts
         accountManager.add(user);
     }
 }
--- Project/Main.java
+++ Project/Main.java
@@ -7,36 +7,80 @@ public class Main {
         Scanner scnr = new Scanner(System.in);
         AccountManager manager = new AccountManager();
         Catalog catalog = new Catalog();
+        Boolean lock = true;
 
-        System.out.println("Login");
-        System.out.print("Username: ");
-        String userName = scnr.next();
-        System.out.print("Password: ");
-        String passWord = scnr.next();
-        Account user = new Account(userName, passWord);
-        manager.addAccount(user);
+        System.out.println("Hello! Would You Like To Login or SignUp?");
+        switch (scnr.next()) {
+            case "Login":
+                System.out.print("Username: ");
+                String userName = scnr.next();
+                System.out.print("Password: ");
+                String passWord = scnr.next();
+                Account login = new Account(userName, passWord);
+                // Search AccountManager for match on username
+                // Check if password matches
+                break;
+                
+            case "SignUp":
+                System.out.println("Please Enter A Name and Password:");
+                System.out.print("Username: ");
+                String newUser = scnr.next();
+                System.out.println();
+                while (lock) {
+                    System.out.println("Password must but at least 6 characters long.");
+                    System.out.print("Password: ");
+                    String newPass = scnr.next();
+                    System.out.println();
+                    if (newPass.length() >= 6) {
+                        Account user = new Account(newUser, newPass);
+                        manager.addAccount(user);
+                        System.out.println("Thank You For Choosing MusicCatalog");
+                        lock = false;
+                    }
+                    else {
+                        System.out.println("Error: Please Enter A New Password.");
+                    }
+                    
+                }
+                break;
+        }
 
         catalog.createCatalog();
         System.out.println();
 
         while (true) {
-            System.out.println("Would You Like To Create a Plalist?");
+            System.out.println("Please Enter What Would You Like To Access: Catalog, Playlist, Exit");
             String input = scnr.next();
-            if (input.equals("Yes") || input.equals("yes")) {
+            if (input.equals("catalog") || input.equals("Catalog")) {
                 System.out.println();
-                System.out.println("Plese Enter Name of Playlist: ");
-                input = scnr.next();
-                user.setPlayName(input);
-            
+                System.out.println("MusicCatalog Songs: ");
                 System.out.println();
-            
+                catalog.displayCatalog();
             }
-            else if (input.equals("No") || input.equals("no")) {
+            else if (input.equals("playlist") || input.equals("Playlist")) {
+                System.out.println("Would You Like To Create, View, or Return?");
+                switch (scnr.next()) {
+                    case "Create" :
+                        System.out.println("Plese Enter Name of Playlist: ");
+                        input = scnr.next();
+                        //user.setPlayName(input);
+        
+                        System.out.println();
+                        break;
+                    case "View" :
+                    case "Return" :
+                        System.out.println();
+                        break;
+                }
+            }
+            else if (input.equals("exit") || input.equals("Exit")) {
                 System.out.println("Would You Like To Logout?");
-                input = scnr.next();
-                if (input.equals("Yes") || input.equals("yes")) {
+                if (scnr.next().equals("Yes") || scnr.next().equals("yes")) {
             	    System.out.println("Application Closed");
-                    break;
+                    return;
+                }
+                else if (scnr.next().equals("no") || scnr.next().equals("No")) {
+                    
                 }
             }
             else {
